2677321913f840ddcb536e3ed535110c7e8dacd2
bing-zhaozhao <1036752192@qq.com>
2023-11-29 21:23:39 +0800

    fs

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+const count = ref(0)
+</script>
+
+<template>
+  <div >asassa </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
